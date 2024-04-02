60e39836891a35bdcded440e3599b44c2274e0c3
Eshwar G Bhore <111167377+Eshwar1002@users.noreply.github.com>
2024-04-02 17:37:46 +0530

    Create Intro.cpp
    First commit

---
+++ Intro.cpp
@@ -0,0 +1,21 @@
+#include <iostream>      //{header file}
+using namespace std;     // The std is a short form of standard, the std namespace contains the built-in classes and declared functions.
+int main() { return 0; } // {This is from where function starts to run.}+++
+
+//  Variable scope: It means where that variable is having scope to be used as local or global scope. OR
+// It is region in code where the existence of variable is valid.
+// Local variables always have the precedence before global variables.
+// I/O stream --> Input and Output stream.
+// "<<" is insertion opoerator & ">>" is Extraction operator.
+
+// System header files
+//  #include <iostream>
+// User defined header files++++
+
+// C++ program to show that we can access a global variable
+// using scope resolution operator " :: " when there is a local
+
+// By defalt decimal digits are taken as double to make them float just add "f" in the end.
+
+// Typecasting:
+//  int &y=x; // Here we are not creating the copy of the variable we are just chaning name of the variable and using it just by different names.
